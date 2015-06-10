1ca6bf88643fabb367b0f193a3704db65a5b7051
Nicolas Hory <hory.nicolas@etu.unice.fr>
2015-06-10 21:47:26 +0200

    Version finale 2

--- my-malloc.c
+++ my-malloc.c
@@ -1,10 +1,10 @@
 /*
- * my-malloc.c 	-- Implementation de malloc, free, calloc, realloc
+ * my-malloc.c  -- Implementation de malloc, free, calloc, realloc
  *
  * Implémentation first-fit pour malloc
  *
  *           Author: Lucas Martinez, Nicolas HORY
- *    		 Version: 26/06/2015
+ *           Version: 26/06/2015
  */
 
 
@@ -14,20 +14,22 @@
 #include "my-malloc.h"
 
 #define HEADER_SIZE sizeof(Header)
-#define BLOCK_SIZE 800 	
+#define BLOCK_SIZE 800  
 #define MOST_RESTRICTING_TYPE double // Pour s’aligner sur des frontieres multiples
-									 // de la taille du type le plus contraignant
+                                     // de la taille du type le plus contraignant
 
 typedef union header { // Header de bloc
-	struct {
-		unsigned int size; // Taille du bloc
-		union header *next; // bloc libre suivant (renommé next)
-	} info;
-	MOST_RESTRICTING_TYPE dummy; // Ne sert qu’a provoquer un alignement
+    struct {
+        unsigned int size; // Taille du bloc
+        union header *next; // bloc libre suivant (renommé next)
+    } info;
+    MOST_RESTRICTING_TYPE dummy; // Ne sert qu’a provoquer un alignement
 } Header;
 
+void *memcpy(void *dest, const void *src, size_t n);
+void *memset(void *s, int c, size_t n);
 void *sbrk(intptr_t increment);
-static Header *base = NULL;				// liste vide pour commencer
+static Header *base = NULL;             // liste vide pour commencer
 static int nb_alloc   = 0;              /* Nombre de fois où on a alloué     */
 static int nb_dealloc = 0;              /* Nombre de fois où on a désalloué  */
 static int nb_sbrk    = 0;              /* Nombre de fois où on a appelé sbrk */
@@ -48,67 +50,67 @@ Header *get_block(void *ptr){
 
 /* Fonction qui cherche un bloc libre pour pouvoir l'allouer avec mymalloc */
 Header *search(Header **last, size_t size){
-	Header *current = base;
-	while (current && (current->info.size < size)) { // tant qu'on a pas trouvé un bloc libre suffisamment grand
-		*last = current;
-		current = current->info.next;
-	}
-	return current;
+    Header *current = base;
+    while (current && (current->info.size < size)) { // tant qu'on a pas trouvé un bloc libre suffisamment grand
+        *last = current;
+        current = current->info.next;
+    }
+    return current;
 }
 
 /* Fonction qui sert à incrémenter le tas lorsqu'il n'y a plus de blocs libres */
 Header *extend_heap(Header *last, size_t size) {
-	Header *block;
-	block = sbrk(0);
-	void *request = sbrk(size + HEADER_SIZE);
+    Header *block;
+    block = sbrk(0);
+    void *request = sbrk(size + HEADER_SIZE);
 
-  	if(request == (void*) -1) {
-  		fprintf(stderr, "No more memory\n");
-  		return NULL; //sbrk failed
-  	}
+    if(request == (void*) -1) {
+        fprintf(stderr, "No more memory\n");
+        return NULL; //sbrk failed
+    }
 
-  	if (last) { //NULL la première fois
-  		last->info.next = block;
-  	}
+    if (last) { //NULL la première fois
+        last->info.next = block;
+    }
 
-  	block->info.size = size;
-  	block->info.next = NULL;
+    block->info.size = size;
+    block->info.next = NULL;
 
-  	nb_sbrk += 1;
+    nb_sbrk += 1;
 
-  	//debug
-  	//printf("---->sbrk @%d, size=%d, next:%d\n", block, block->info.size, block->info.next);
+    //debug
+    //printf("---->sbrk @%d, size=%d, next:%d\n", block, block->info.size, block->info.next);
 
-  	printf("---->sbrk(%d)\n", size);
+    printf("---->sbrk(%zu)\n", size);
 
-  	
-  	return block;
+    
+    return block;
 }
 
 /* Fonction qui coupe un bloc en deux lors que le bloc alloué est plus grand que la taille nécessaire */
 void split(Header *block, size_t size){
-	Header *new_block;
+    Header *new_block;
 
-	new_block = get_ptr(block) + size;
-	new_block->info.size = block->info.size - (size + HEADER_SIZE);
-	new_block->info.next = block->info.next;
+    new_block = get_ptr(block) + size;
+    new_block->info.size = block->info.size - (size + HEADER_SIZE);
+    new_block->info.next = block->info.next;
 
-	block->info.size = size;
-	block->info.next = new_block;
+    block->info.size = size;
+    block->info.next = new_block;
 
-	//debug
-	//printf("-------split------- \nusedblock @0x%d, size=%d, next->0x%d\n", block, block->info.size, block->info.next);
-	//printf("freeblock @0x%d, size=%d, next->0x%d\n", new_block, new_block->info.size, new_block->info.next);
+    //debug
+    //printf("-------split------- \nusedblock @0x%d, size=%d, next->0x%d\n", block, block->info.size, block->info.next);
+    //printf("freeblock @0x%d, size=%d, next->0x%d\n", new_block, new_block->info.size, new_block->info.next);
 }
 
 /* Fonction qui supprime le bloc de la liste des blocs libres */
 void remove_block(Header *block) {
     if (block == base) {
-    	if (block->info.next) {
-    		base = block->info.next;
-    	} else {
-    		base = NULL;
-    	}
+        if (block->info.next) {
+            base = block->info.next;
+        } else {
+            base = NULL;
+        }
         return;
     }
 
@@ -119,102 +121,102 @@ void remove_block(Header *block) {
         }
         
         if(new_block->info.next) {
-        	new_block->info.next = new_block->info.next->info.next;
+            new_block->info.next = new_block->info.next->info.next;
         } else {
-        	new_block->info.next = NULL;
+            new_block->info.next = NULL;
         }
     }
 }
 
 /* Fonction qui alloue de la mémoire */
 void *mymalloc(size_t size) {
-	printf("mymalloc(%d)\n", size);
+    printf("mymalloc(%zu)\n", size);
 
-	Header *block;
+    Header *block;
 
-	if (size <= 0) {
-		return NULL;
-	}
+    if (size <= 0) {
+        return NULL;
+    }
 
-	if (!base) { // premier appel
-		block = extend_heap(NULL, BLOCK_SIZE);
-		if (!block) {
-		  	return NULL;
-		}
+    if (!base) { // premier appel
+        block = extend_heap(NULL, BLOCK_SIZE);
+        if (!block) {
+            return NULL;
+        }
 
-		base = block;
-	} 
+        base = block;
+    } 
 
-	//ensuite
-	if (base) {
-		Header *last = base;
-		block = search(&last, size);
+    //ensuite
+    if (base) {
+        Header *last = base;
+        block = search(&last, size);
 
-		if (!block) { // on n'a pas trouvé de bloc libre
-		  	block = extend_heap(last, BLOCK_SIZE); //alors on agrandit le tas
-		  	if (!block) {
-		    	return NULL; //NULL si extend_heap n'a pas marché
-		  	}
-		} 
+        if (!block) { // on n'a pas trouvé de bloc libre
+            block = extend_heap(last, BLOCK_SIZE); //alors on agrandit le tas
+            if (!block) {
+                return NULL; //NULL si extend_heap n'a pas marché
+            }
+        } 
 
-		if (block) { //on a trouvé un bloc libre
-		  	if (block->info.size >= (HEADER_SIZE + size)){ //si la taille du bloc est plus grande qu'un header + size octets
-				split(block, size); //alors on split
-			}
-		}
-	}
+        if (block) { //on a trouvé un bloc libre
+            if (block->info.size >= (HEADER_SIZE + size)){ //si la taille du bloc est plus grande qu'un header + size octets
+                split(block, size); //alors on split
+            }
+        }
+    }
 
-	//debug
-	//printf("mymalloc in block @0x%d, size=%d, next->0x%d\n", block, block->info.size, block->info.next);
+    //debug
+    //printf("mymalloc in block @0x%d, size=%d, next->0x%d\n", block, block->info.size, block->info.next);
 
-	nb_alloc += 1;
-	remove_block(block); //enlève le bloc de la liste des blocs libres
-	return get_ptr(block); // car on renvoit le pointeur sur la région après notre bloc (soit block + HEADER_SIZE)
+    nb_alloc += 1;
+    remove_block(block); //enlève le bloc de la liste des blocs libres
+    return get_ptr(block); // car on renvoit le pointeur sur la région après notre bloc (soit block + HEADER_SIZE)
 }
 
 /* Fonction qui fusionne deux blocs qui se suivent si possible */
 void fusion(Header *block){
-	Header *next_block = block->info.next;
-	if (next_block) {
-		if ((get_ptr(block) + block->info.size) == next_block) {
-			block->info.size += HEADER_SIZE + next_block->info.size;
-			block->info.next = next_block->info.next;
-		}
-	}
+    Header *next_block = block->info.next;
+    if (next_block) {
+        if ((get_ptr(block) + block->info.size) == next_block) {
+            block->info.size += HEADER_SIZE + next_block->info.size;
+            block->info.next = next_block->info.next;
+        }
+    }
 }
 
 /* Fonction qui libère la mémoire allouée précedemment par mymalloc */
 void myfree(void *ptr) {
-	nb_dealloc += 1;
-	Header *block_ptr = get_block(ptr);
+    nb_dealloc += 1;
+    Header *block_ptr = get_block(ptr);
 
-	if (!base) {
-		base = block_ptr;
-	} else {
-		if (block_ptr < base) { // nouvelle base
-			block_ptr->info.next = base;
-			base = block_ptr;
-			fusion(base); //fusion avec le suivant
+    if (!base) {
+        base = block_ptr;
+    } else {
+        if (block_ptr < base) { // nouvelle base
+            block_ptr->info.next = base;
+            base = block_ptr;
+            fusion(base); //fusion avec le suivant
 
-			//debug
-			//printf("myfree in block @0x%d, size=%d, next->0x%d\n", block_ptr, block_ptr->info.size, block_ptr->info.next);
+            //debug
+            //printf("myfree in block @0x%d, size=%d, next->0x%d\n", block_ptr, block_ptr->info.size, block_ptr->info.next);
 
-			return;
-		}
+            return;
+        }
 
-		Header *block = base;
-		while (block->info.next && block->info.next != block && block->info.next < block_ptr) {
-			block = block->info.next;
-		}
+        Header *block = base;
+        while (block->info.next && block->info.next != block && block->info.next < block_ptr) {
+            block = block->info.next;
+        }
 
-		block_ptr->info.next = block->info.next;
-		block->info.next = block_ptr;
-		fusion(block_ptr); //fusion avec le suivant
-		fusion(block); //fusion avec le précédent
-	}
+        block_ptr->info.next = block->info.next;
+        block->info.next = block_ptr;
+        fusion(block_ptr); //fusion avec le suivant
+        fusion(block); //fusion avec le précédent
+    }
 
-	//debug
-	//printf("myfree in block @0x%d, size=%d, next->0x%d\n", block_ptr, block_ptr->info.size, block_ptr->info.next);
+    //debug
+    //printf("myfree in block @0x%d, size=%d, next->0x%d\n", block_ptr, block_ptr->info.size, block_ptr->info.next);
 }
 
 /* Fonction qui change la taille du bloc pointé par ptr par la taille size. Le contenu n'est pas changé si la nouvelle
@@ -223,25 +225,25 @@ void myfree(void *ptr) {
 
 // ERREUR DANS LE TEST ./test-malloc 10 100 : boucle infinie, Résolu
 void *myrealloc(void *ptr, size_t size) {
-	printf("myrealloc(%d)\n", size);
-	if (!ptr) {
-		return mymalloc(size); // ptr == NULL donc realloc doit être équivalent à malloc(size)
-	}
-    	nb_alloc+=1;
-	Header *block_ptr = get_block(ptr); // Récupère le bloc souhaité
-	if (block_ptr->info.size >= size) { //si le bloc est plus grand que la nouvelle taille
-		if ((block_ptr->info.size - size) >= (HEADER_SIZE + 4)){ //si la taille du bloc est plus grande qu'un header + 4 octets (bloc minimal)
-			return mymalloc(size);
-		}
-		return ptr;
-	}
+    printf("myrealloc(%zu)\n", size);
+    if (!ptr) {
+        return mymalloc(size); // ptr == NULL donc realloc doit être équivalent à malloc(size)
+    }
+        nb_alloc+=1;
+    Header *block_ptr = get_block(ptr); // Récupère le bloc souhaité
+    if (block_ptr->info.size >= size) { //si le bloc est plus grand que la nouvelle taille
+        if ((block_ptr->info.size - size) >= (HEADER_SIZE + 4)){ //si la taille du bloc est plus grande qu'un header + 4 octets (bloc minimal)
+            return mymalloc(size);
+        }
+        return ptr;
+    }
 
-	//sinon il faut allouer un nouvel espace mémoire, copier les données et libérer l'ancien bloc
-	void *new_ptr = mymalloc(size);
-	if (!new_ptr) {
-		return NULL;
-	}
-	memcpy(new_ptr, ptr, block_ptr->info.size); //copie "size" octets de ptr vers new_ptr
+    //sinon il faut allouer un nouvel espace mémoire, copier les données et libérer l'ancien bloc
+    void *new_ptr = mymalloc(size);
+    if (!new_ptr) {
+        return NULL;
+    }
+    memcpy(new_ptr, ptr, block_ptr->info.size); //copie "size" octets de ptr vers new_ptr
     //myfree(ptr); 
     return new_ptr;
 }
@@ -249,37 +251,37 @@ void *myrealloc(void *ptr, size_t size) {
 /* Fonction qui alloue la mémoire nécessaire pour un tableau de nb_elem éléments de taille elem_size octets, et renvoie un pointeur 
 ** vers la mémoire allouée. Cette zone est remplie avec des zéros. Si nb_elem ou elem_size vaut 0, calloc() renvoie NULL. */
 void *mycalloc(size_t nb_elem, size_t elem_size) {
-	if(nb_elem == 0 || elem_size == 0){
-		return NULL;
-	}
+    if(nb_elem == 0 || elem_size == 0){
+        return NULL;
+    }
 
-	size_t size = nb_elem * elem_size; // Attention à l'overflow (pas vérifié ici)
-	void *ptr = mymalloc(size);
-	if (ptr){
-		memset(ptr, 0, size); //met les données pointées par ptr à 0, jusqu'à "size"
-	}
+    size_t size = nb_elem * elem_size; // Attention à l'overflow (pas vérifié ici)
+    void *ptr = mymalloc(size);
+    if (ptr){
+        memset(ptr, 0, size); //met les données pointées par ptr à 0, jusqu'à "size"
+    }
 
-	return ptr;
+    return ptr;
 }
 
 
 #ifdef MALLOC_DBG
 void mymalloc_infos(char *msg) {
-	if (msg) fprintf(stderr, "**********\n*** %s\n", msg);
+    if (msg) fprintf(stderr, "**********\n*** %s\n", msg);
 
-	fprintf(stderr, "# allocs = %3d - # deallocs = %3d - # sbrk = %3d\n",
-			nb_alloc, nb_dealloc, nb_sbrk);
-	/* Ca pourrait être pas mal d'afficher ici les blocs dans la liste libre */
-	if (base)
+    fprintf(stderr, "# allocs = %3d - # deallocs = %3d - # sbrk = %3d\n",
+            nb_alloc, nb_dealloc, nb_sbrk);
+    /* Ca pourrait être pas mal d'afficher ici les blocs dans la liste libre */
+    if (base)
     {
-    	fprintf(stderr, "List of free blocks:\n");
+        fprintf(stderr, "List of free blocks:\n");
         for (Header *block = base; block && block != block->info.next; block = block->info.next)
         {
-            fprintf(stderr, "Block @0x%d (size=%d, next->0x%d)\n", block, block->info.size, block->info.next);
+            fprintf(stderr, "Block %p (size=%d, next->%p)\n", block, block->info.size, block->info.next);
         }
     }
 
-	if (msg) fprintf(stderr, "**********\n\n");
+    if (msg) fprintf(stderr, "**********\n\n");
 
 }
 #endif
